395f696abb5e6f2fe7ad257883472bac4a6e2803
0x41gawor <41gawor@gmail.com>
2022-06-08 20:43:09 +0200

    feat: create enemy class

---
+++ enemy.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+)
+
+type Enemy struct {
+	bodyBlack, bodyWhite, bodyGreen *ebiten.Image
+	pos                             Vector
+	sizeX, sizeY                    int
+	ms                              float64
+	health, maxHealth               float32
+}
+
+func NewEnemy(sizeX int, sizeY int, ms float64, health float32, maxHealth float32) Enemy {
+	imageWhite := ebiten.NewImage(sizeX+2, sizeY+2)
+	imageWhite.Fill(color.White)
+	imageBlack := ebiten.NewImage(sizeX, sizeY)
+	imageBlack.Fill(color.Black)
+	imageGreen := ebiten.NewImage(sizeX, sizeY)
+	imageGreen.Fill(color.RGBA{G: 0xff, A: 0xff})
+
+	return Enemy{
+		bodyBlack: imageBlack,
+		bodyWhite: imageWhite,
+		bodyGreen: imageGreen,
+		pos:       Vector{X: 50, Y: 50},
+		sizeX:     sizeX,
+		sizeY:     sizeY,
+		ms:        ms,
+		health:    health,
+		maxHealth: maxHealth,
+	}
+}
+
+func (e *Enemy) Draw(screen *ebiten.Image) {
+	opt := &ebiten.DrawImageOptions{}
+	opt.GeoM.Translate(e.pos.X, e.pos.Y)
+	screen.DrawImage(e.bodyWhite, opt)
+	opt.GeoM.Translate(1, 1)
+	screen.DrawImage(e.bodyBlack, opt)
+	opt.GeoM.Translate(0, float64((1-e.health/e.maxHealth)*float32(sizeY)))
+	screen.DrawImage(e.bodyGreen, opt)
+}
+
+func (e *Enemy) Update() {
+	e.pos.X += 0.0
+	e.pos.Y += 0.0
+	e.bodyGreen = ebiten.NewImage(sizeX+2, int(float32(sizeY)*e.health/e.maxHealth))
+	e.bodyGreen.Fill(color.RGBA{G: 0xff, A: 0xff})
+}
--- game.go
+++ game.go
@@ -12,22 +12,26 @@ const (
 
 type Game struct {
 	player Player
+	enemy  Enemy
 }
 
 func NewGame() Game {
 	ebiten.SetWindowSize(SCREENWIDTH, SCREENHEIGHT)
 	ebiten.SetWindowTitle(TITLE)
 	var player = NewPlayer()
-	return Game{player: player}
+	var enemy = NewEnemy(30, 50, 0, 80, 100)
+	return Game{player: player, enemy: enemy}
 }
 
 func (g *Game) Update() error {
 	g.player.Update()
+	g.enemy.Update()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
+	g.enemy.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
